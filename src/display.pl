6a430e9904086272bee9a0c01cadf4d0de4a1d86
pedrovponte <up201809694@fe.up.pt>
2020-10-28 15:36:16 +0000

    Add scorer to the board and add player turn info

--- src/display.pl
+++ src/display.pl
@@ -1,11 +1,17 @@
 initialBoard([
+[1,2,3,4,5,6,7],
+[8,9,rs,emptyS,p,n,t],
+[emptyS,emptyS,emptyS,emptyS,emptyS,emptyS,emptyS],
 [red,empty,empty,empty,empty,empty,red],
 [empty,empty,empty,empty,empty,empty,empty],
 [empty,empty,empty,empty,empty,empty,empty],
 [empty,empty,empty,empty,empty,empty,empty],
 [empty,empty,empty,empty,empty,empty,empty],
 [empty,empty,empty,empty,empty,empty,empty],
-[yellow,empty,empty,empty,empty,empty,yellow]
+[yellow,empty,empty,empty,empty,empty,yellow],
+[emptyS,emptyS,emptyS,emptyS,emptyS,emptyS,emptyS],
+[1,2,3,4,5,6,7],
+[8,9,ys,emptyS,p,n,t]
 ]).
 
 print_board(X) :-
@@ -31,6 +37,21 @@ print_cell(C):-
 	code(C,P), write(P), write(' |').
 	
 code(empty, '  ').
+code(emptyS, '--').
 code(red, 'RF').
 code(yellow, 'YF').
 code(stone, 'SS').
+code(1, ' 1').
+code(2, ' 2').
+code(3, ' 3').
+code(4, ' 4').
+code(5, ' 5').
+code(6, ' 6').
+code(7, ' 7').
+code(8, ' 8').
+code(9, ' 9').
+code(p, ' P').
+code(n, ' N').
+code(t, ' T').
+code(rs, 'RS').
+code(ys, 'YS').
--- src/play.pl
+++ src/play.pl
@@ -1,9 +1,20 @@
+% main function, that initializes the game and calls the game loop
 play :-
 	initial(GameState),
-	display_game(GameState, Player).
+	display_game(GameState, Player),
+	start_game(GameState).
 
+% function to create the board
 initial(GameState):-
 	initialBoard(GameState).
 	
+% function to display the board
 display_game(GameState, Player):-
 	print_board(GameState).
+
+% by now, yellow player is the first to play, but in the future we can put it random
+start_game(GameState) :-
+	turn(GameState, 'Yellow Player').
+
+turn(GameState, Player) :-
+	format('\n ~a turn.\n\n', Player).
